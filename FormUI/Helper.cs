ab2fadfa3f6b20932cfd68ad49efd966378570fb
Cigir Temelkuran <91200000641@ogrenci.ege.edu.tr>
2021-06-14 20:46:16 +0300

    app. config changed

---
+++ FormUI/Helper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormUI
+{
+    public static class Helper
+    {
+        // When we say Helper.CnnVal("name of the configstring") it will look to app.config for that connection string
+        public static string CnnVal(string name)
+        {
+            return ConfigurationManager.ConnectionStrings[name].ConnectionString;
+        }
+    }
+}
